2238ac395171d0af73ccf5b8c9c24351fb6987b9
fantajin <fantajin0350@gmail.com>
2022-09-17 18:34:01 +0900

    grayscale false

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def upload_file():
             filepath = os.path.join(UPLOAD_FOLDER, filename)
 
             #受け取った画像を読み込み、np形式に変換
-            img = image.load_img(filepath, grayscale=True, target_size=(image_size,image_size))
+            img = image.load_img(filepath, grayscale=False, target_size=(image_size,image_size,3))
             img = image.img_to_array(img)
             data = np.array([img])
             
